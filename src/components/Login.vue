cdba764446ac3c6628faa2ef9323102563632f4f
Mustafa Dokumacı <mustafa@foraa.com>
2019-01-25 10:30:46 +0100

    add signup and login forms

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,39 @@
 <template>
+  <div>
+    <div>
+      <label for="email">E-mail</label>
+      <input type="text" id="email" v-model="email" />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+    <div>
+      <button @click="login">Login</button>
+      <router-link to="/signup">Sign up</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
   props: [ 'user' ],
   watch: {
     'user.type': function (type) {
       if (type === 'real') {
         this.$router.replace('/me')
       }
+    }
+  },
+  methods: {
+    login() {
+
     }
   },
   created() {
---
+++ src/components/Signup.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>
+      <label for="email">E-mail</label>
+      <input type="text" id="email" v-model="email" />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+    <div>
+      <label for="password2">Repeat Password</label>
+      <input type="password" id="password2" v-model="password2" />
+    </div>
+    <div>
+      <button @click="signup">Sign up</button>
+      <router-link to="/login">Login</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      password2: ''
+    }
+  },
+  props: [ 'user' ],
+  watch: {
+    'user.type': function (type) {
+      if (type === 'real') {
+        this.$router.replace('/me')
+      }
+    }
+  },
+  methods: {
+    signup() {
+
+    }
+  },
+  created() {
+    if (this.user.type === 'real') {
+        this.$router.replace('/me')
+    }
+  }
+}
+</script>
