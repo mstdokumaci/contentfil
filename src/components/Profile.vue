322a9d92b4d053750de2a301e1dbe8dc2bbb1274
Mustafa Dokumacı <mustafa@foraa.com>
2019-04-10 16:40:49 +0200

    little refactor and bugfix

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -57,14 +57,15 @@ export default {
 
     subscription = this.$client.get('draft', {}).subscribe(list => {
       this.list = list.map((item, key) => {
-        const el = document.createElement('div')
-        el.innerHTML = item.get('content').compute()
-        return {
-          key: key,
-          title: el.firstChild.textContent,
-          published: item.get('published').compute()
+        if (item.get('content') !== void 0) {
+          const el = document.createElement('div')
+          el.innerHTML = item.get('content').compute()
+          return {
+            key: key,
+            title: el.firstChild ? el.firstChild.textContent : key
+          }
         }
-      })
+      }).filter(item => item)
     })
   },
   destroyed: () => subscription && subscription.unsubscribe()
--- src/server/index.js
+++ src/server/index.js
@@ -1,85 +1,10 @@
 const { create } = require('stx')
 const crypto = require('crypto')
 
-const auth = create()
-
-setErrorStatus = user => {
-  if (user.get('type') !== void 0) {
-    user.set({ status: 'error' })
-  }
-}
-
-const createUser = (user, _, branchUser) => {
-  try {
-    user = JSON.parse(user)
-  } catch (e) {
-    return setErrorStatus(branchUser)
-  }
-  const { email, password } = user
-  if (auth.get(email) !== void 0) {
-    return setErrorStatus(branchUser)
-  }
-  const salt = crypto.randomBytes(64)
-  const hash = crypto.scryptSync(password, salt, 64).toString('base64')
-  auth.set({
-    [ email ]: {
-      salt: salt.toString('base64'),
-      hash
-    }
-  })
-  branchUser.set({ status: 'created' })
-}
-
-const authByPassword = (email, password, switcher) => {
-  const user = auth.get(email)
-  if (user !== void 0) {
-    const salt = Buffer.from(user.get('salt').compute(), 'base64')
-    const hash = crypto.scryptSync(password, salt, 64).toString('base64')
-    if (user.get('hash').compute() === hash) {
-      const tokenExpiresAt = user.get('tokenExpiresAt')
-      let token
-      if (
-        tokenExpiresAt !== void 0
-        && tokenExpiresAt.compute() > Date.now()
-      ) {
-        token = user.get('token').compute()
-      } else {
-        token = crypto.randomBytes(64).toString('base64')
-        user.set({
-          token,
-          tokenExpiresAt: Date.now() + 86400 * 1000 * 10
-        })
-      }
-      switcher(email).get('user').set({
-        type: 'real',
-        email,
-        token,
-        tokenExpiresAt: user.get('tokenExpiresAt').compute()
-      })
-      return true
-    }
-  }
-}
-
-const authByToken = (email, token, switcher) => {
-  const user = auth.get(email)
-  if (user !== void 0) {
-    const tokenExpiresAt = user.get('tokenExpiresAt')
-    if (
-      tokenExpiresAt !== void 0
-      && tokenExpiresAt.compute() > Date.now()
-      && user.get('token').compute() === token 
-    ) {
-      switcher(email).get('user').set({
-        type: 'real',
-        email,
-        token,
-        tokenExpiresAt: tokenExpiresAt.compute()
-      })
-      return true
-    }
-  }
-}
+const {
+  createUser,
+  switchBranch
+} = require('./auth')
 
 const createDraft = (_, __, branchDraft) => {
   const timeBuf = Buffer.allocUnsafe(4)
@@ -94,7 +19,7 @@ const createDraft = (_, __, branchDraft) => {
 
   branchDraft.set({
     [ id ]: {
-      content: ''
+      content: '',
     }
   })
   branchDraft.root().get('route').set(`/draft/${id}`)
@@ -137,41 +62,4 @@ contentfil.branch.newBranchMiddleware = newBranch => {
 }
 
 const server = contentfil.listen(7071)
-server.switchBranch = (fromBranch, branchKey, switcher) => {
-  let authRequest
-  const branchUser = fromBranch.get('user')
-  try {
-    authRequest = JSON.parse(branchKey)
-  } catch (e) {
-    return setErrorStatus(branchUser)
-  }
-  if (!authRequest.type) {
-    setErrorStatus(branchUser)
-  } else if (
-    authRequest.type === 'anonymous'
-    && authRequest.id
-  ) {
-    switcher(authRequest.id).get('user').set({
-      type: 'anonymous',
-      id: authRequest.id
-    })
-  } else if (
-    authRequest.type === 'token'
-    && authRequest.email
-    && authRequest.token
-  ) {
-    if (!authByToken(authRequest.email, authRequest.token, switcher)) {
-      setErrorStatus(branchUser)
-    }
-  } else if (
-    authRequest.type === 'password'
-    && authRequest.email
-    && authRequest.password
-  ) {
-    if (!authByPassword(authRequest.email, authRequest.password, switcher)) {
-      setErrorStatus(branchUser)
-    }
-  } else {
-    setErrorStatus(branchUser)
-  }
-}
+server.switchBranch = switchBranch
