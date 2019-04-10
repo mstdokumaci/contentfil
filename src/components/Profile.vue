14369657295aa084dda9172f94b1e5986a698309
Mustafa Dokumacı <mustafa@foraa.com>
2019-04-10 16:43:59 +0200

    revert unnecessary

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -57,13 +57,11 @@ export default {
 
     subscription = this.$client.get('draft', {}).subscribe(list => {
       this.list = list.map((item, key) => {
-        if (item.get('content') !== void 0) {
-          const el = document.createElement('div')
-          el.innerHTML = item.get('content').compute()
-          return {
-            key: key,
-            title: el.firstChild ? el.firstChild.textContent : key
-          }
+        const el = document.createElement('div')
+        el.innerHTML = item.get('content').compute()
+        return {
+          key: key,
+          title: el.firstChild ? el.firstChild.textContent : key
         }
       }).filter(item => item)
     })
