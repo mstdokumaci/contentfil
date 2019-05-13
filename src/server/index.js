aa19620b81a4d1c90dd7b8bd1ac9dac8407778ef
Mustafa Dokumaci <mustafa@foraa.com>
2019-05-13 13:22:57 +0200

    restructure server side code for story publishing

--- src/server/index.js
+++ src/server/index.js
@@ -1,55 +1,23 @@
 const { create } = require('stx')
-const crypto = require('crypto')
 
 const {
   createUser,
   switchBranch
 } = require('./auth')
 
-const createDraft = (_, __, branchDraft) => {
-  const timeBuf = Buffer.allocUnsafe(4)
-  timeBuf.writeUInt32BE((Date.now() & 0xffffffff) >>> 0, 0)
-  const id = Buffer.concat([
-    crypto.randomBytes(12),
-    timeBuf
-  ]).toString('base64')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=+$/, '')
-
-  branchDraft.set({
-    [ id ]: {
-      content: ''
-    }
-  })
-  branchDraft.root().get('route').set(`/draft/${id}`)
-}
-
-const publishDraft = (id, __, branchDraft) => {
-  const draft = branchDraft.get(id)
-  if (draft === void 0) {
-    return
-  }
-  contentfil.get('published').set({
-    [ id ]: {
-      content: draft.get('content').compute(),
-      date: Date.now(),
-      author: branchDraft.root().get([ 'user', 'email' ]).compute()
-    }
-  })
-  draft.set({
-    published: [ '@', 'published', id ]
-  })
-}
-
-const contentfil = create({
+const master = create({
   user: {},
   route: '',
   draft: {},
   published: {}
 })
 
-contentfil.branch.newBranchMiddleware = newBranch => {
+const {
+  createDraft,
+  publishDraft
+} = require('./story')(master)
+
+master.branch.newBranchMiddleware = newBranch => {
   newBranch.get('user').on('create', createUser)
   newBranch.get('draft').on('create', createDraft)
   newBranch.get('draft').on('publish', publishDraft)
@@ -63,5 +31,5 @@ contentfil.branch.newBranchMiddleware = newBranch => {
   ]
 }
 
-const server = contentfil.listen(7071)
+const server = master.listen(7071)
 server.switchBranch = switchBranch
---
+++ src/server/story.js
@@ -0,0 +1,42 @@
+const crypto = require('crypto')
+
+const createDraft = (_, __, branchDraft) => {
+  const timeBuf = Buffer.allocUnsafe(4)
+  timeBuf.writeUInt32BE((Date.now() & 0xffffffff) >>> 0, 0)
+  const id = Buffer.concat([
+    crypto.randomBytes(12),
+    timeBuf
+  ]).toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '')
+
+  branchDraft.set({
+    [ id ]: {
+      content: ''
+    }
+  })
+  branchDraft.root().get('route').set(`/draft/${id}`)
+}
+
+const publishDraft = (master, id, __, branchDraft) => {
+  const draft = branchDraft.get(id)
+  if (draft === void 0) {
+    return
+  }
+  master.get('published').set({
+    [ id ]: {
+      content: draft.get('content').compute(),
+      date: Date.now(),
+      author: branchDraft.root().get([ 'user', 'email' ]).compute()
+    }
+  })
+  draft.set({
+    published: [ '@', 'published', id ]
+  })
+}
+
+module.exports = master => ({
+  createDraft,
+  publishDraft: publishDraft.bind(null, master)
+})
