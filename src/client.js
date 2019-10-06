8d8f21b32809675ce53ee26c15c4836239d80356
Mustafa Dokumaci <mustafa@foraa.com>
2019-10-06 13:00:53 +0200

    disconnect when not visible

--- src/client.js
+++ src/client.js
@@ -1,6 +1,27 @@
 import { create } from 'stx/dist/es'
 
 const client = create()
-client.connect('ws://localhost:7071')
+let connection = client.connect('ws://localhost:7071')
+let connected
+let disconnectTimeout
+
+client.on('connected', status => {
+  connected = status
+})
+
+document.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'hidden') {
+    if (connected) {
+      disconnectTimeout = setTimeout(
+        () => connection.socket.close(), 3000
+      )
+    }
+  } else {
+    clearTimeout(disconnectTimeout)
+    if (!connected) {
+      connection = client.connect('ws://localhost:7071')
+    }
+  }
+})
 
 export default client
